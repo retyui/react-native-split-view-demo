b406623a0bbc78f52661ee863456cd495639d717
David <4661784+retyui@users.noreply.github.com>
2020-05-29 11:17:26 +0300

    Init react-navigation

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+
+function App() {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+}
+
+export default App;
